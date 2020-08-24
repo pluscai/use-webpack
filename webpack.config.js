81203e3b70fe04d07f4dca63b63de21f2069e2e4
pluscai <3114658034@qq.com>
2020-08-24 11:24:47 +0800

    3-2 使用Loader打包静态资源（图片篇）

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,15 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.png$/,
+            test: /\.(png|png|gif)$/,
             use: {
-                loader: "file-loader"
+                loader: "url-loader",
+                options: {
+                    // 下面[name]这种写法就是placeholder-占位符
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'images/',
+                    limit: 20480 // 2048=1k 图片小于10k就转base64
+                }
             }
         }]
     },
