32ffc3f37f04f92cf499468e5b69ec3ef8ec34d8
pluscai <3114658034@qq.com>
2020-08-26 15:27:37 +0800

    3-12 使用Babel处理ES6语法（2）

--- webpack.config.js
+++ webpack.config.js
@@ -20,12 +20,7 @@ module.exports = {
         rules: [{ 
                 test: /\.js$/, 
                 exclude: /node_modules/, 
-                loader: "babel-loader",
-                options: {
-                    presets: [["@babel/preset-env",{
-                        useBuiltIns: 'usage'
-                    }]]
-                } 
+                loader: "babel-loader"
             },{
                 test: /\.(png|png|gif)$/,
                 use: {
