a9a00930157f9e2f767d5ff8d0f0b330df05d268
pluscai <3114658034@qq.com>
2020-08-27 18:29:38 +0800

    4-2 Development和Production模式的区分打包

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,20 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-    mode: 'production',
-    devtool: 'cheap-module-source-map',
     entry: {
         main: './src/index.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        open: true,
-        port: 8080,
-        hot: true,
-        hotOnly: true
     },
     module: {
         rules: [{ 
@@ -61,11 +51,7 @@ module.exports = {
             template: 'src/index.html'
          }),
         new CleanWebpackPlugin(),
-        new webpack.HotModuleReplacementPlugin()
     ],
-    // optimization: {
-    //     usedExports: true,
-    //  },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { add } from './math.js'
 
-add(1, 2);
+add(1, 3);
 
