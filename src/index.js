a3fe1c7b533fdb5ec621a7227262f627f62da6fc
pluscai <3114658034@qq.com>
2020-08-28 14:41:47 +0800

    4-5&6 SplitChunksPlugin配置参数详解

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function getComponent() {
-    return import('lodash').then(({ default: _ }) => {
-        var element = document.createElement('div');
-        element.innerHTML = _.join(['c', 's', 'x'], '-');
-        return element;
-    })
-}
+import _ from 'lodash';
+import test from './test.js'
 
-// 异步获取lodash库
-getComponent().then(element => {
-    document.body.appendChild(element);
-})
+console.log(test.name);
+
+var element = document.createElement('div');
+element.innerHTML = _.join(['c', 's', 'x'], '-');
+document.body.appendChild(element);
+
+// function getComponent() {
+//     return import('lodash').then(({ default: _ }) => {
+//         var element = document.createElement('div');
+//         element.innerHTML = _.join(['c', 's', 'x'], '-');
+//         return element;
+//     })
+// }
+
+// // 异步获取lodash库
+// getComponent().then(element => {
+//     document.body.appendChild(element);
+// })
