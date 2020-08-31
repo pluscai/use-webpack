decb540a79c064d972aaba846b5208e23f7980e2
pluscai <3114658034@qq.com>
2020-08-31 10:56:53 +0800

    4-8 preload做性能优化

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -54,25 +54,7 @@ module.exports = {
     ],
     optimization: {
         splitChunks: {
-          chunks: 'all',  // 同步异步代码都做代码分割
-          minSize: 20000,  // 超过20k才做代码分割
-          maxSize: 0,
-          minChunks: 1, // 被引用的次数超过1次才做代码分割，否则不做代码分割
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          automaticNameDelimiter: '~', 
-          enforceSizeThreshold: 50000,
-          cacheGroups: {
-            vendors: {  // 如果进行代码分割的模块在node_modules（即使第三方类库），则默认加个vendors前缀
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,  // 代码分割时应用该策略的优先级，越大优先级越高
-            },
-            default: { // 不是厂商类库，又符合上面的20k、被引用过至少一次的代码被分割打包到common.js里
-              priority: -20,
-              reuseExistingChunk: true,
-              filename: 'common.js'
-            }
-          }
+          chunks: 'all' // 同步异步代码都做代码分割
         }
     },
     output: {
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-// import _ from 'lodash';
-
-// var element = document.createElement('div');
-// element.innerHTML = _.join(['c', 's', 'x'], '-');
-// document.body.appendChild(element);
-
-function getComponent() {
-    return import('lodash').then(({ default: _ }) => {
-        var element = document.createElement('div');
-        element.innerHTML = _.join(['c', 's', 'x'], '-');
-        return element;
-    })
-}
-
-// 页面一开始不加载 0.js  只有在点击页面时，才会去加载 0.js，并在页面上打印出 csx
 document.addEventListener('click', () => {
-    // 异步获取lodash库
-    getComponent().then(element => {
-        document.body.appendChild(element);
-    })
+   import(/* webpackPrefetch: true */ './click.js').then(({default: func}) => {
+       func();
+   })
 });
 
